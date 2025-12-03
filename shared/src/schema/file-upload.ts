01e6927d0c3ac8e2f8563d064b781f96f129bb1d
zwnkoko <zwnkoko@gmail.com>
2025-12-03 16:07:19 +0800

    config(file-upload-schema): add limit to number of files allowed for upload

--- shared/src/schema/file-upload.ts
+++ shared/src/schema/file-upload.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const FileUploadConfigSchema = z.object({
   allowedMimeTypes: z.record(z.string(), z.array(z.string())),
   maxFileSizeInMB: z.literal(5),
+  maxFiles: z.number().int().positive(),
 });
 
 export type FileUploadConfig = z.infer<typeof FileUploadConfigSchema>;
@@ -10,4 +11,5 @@ export type FileUploadConfig = z.infer<typeof FileUploadConfigSchema>;
 export const FILE_UPLOAD_CONFIG = FileUploadConfigSchema.parse({
   allowedMimeTypes: { "image/*": [] },
   maxFileSizeInMB: 5,
+  maxFiles: 6,
 } as const);
